455865072cf0d13a98e2acc30d1ddb02e031ce80
Shyadha Fathima <165204122+ShyadhaJamimullah@users.noreply.github.com>
2026-05-12 16:15:30 +0530

    Added logging for producer and consumer (#4)

--- scripts/kafka_consumer_to_mysql.py
+++ scripts/kafka_consumer_to_mysql.py
@@ -66,7 +66,7 @@ def consume_snapshot_events(snapshot_date):
 
     cursor.execute(delete_query, (snapshot_date,))
 
-    inserted_count = 0
+    inserted_count =0
 
     for message in consumer:
         event = message.value
